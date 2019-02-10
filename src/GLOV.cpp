18faf983342d43e19c2ea7efffce3cfcd18ba23d
Adam Sawicki <sawickiap@poczta.onet.pl>
2019-02-10 21:41:39 +0100

    Added Vulkan Memory Allocator library, Adjusted project settings (C++17, warning level 4, floating point fast).

--- src/GLOV.cpp
+++ src/GLOV.cpp
@@ -1,7 +1,19 @@
 #include "../include/GLOV.h"
 
+#define NOMINMAX // For Windows.h
+
 #define VK_USE_PLATFORM_WIN32_KHR
 #include <vulkan/vulkan.h>
+
+#pragma warning(push, 4)
+#pragma warning(disable: 4127) // conditional expression is constant
+#pragma warning(disable: 4100) // unreferenced formal parameter
+#pragma warning(disable: 4189) // local variable is initialized but not referenced
+
+#define VMA_IMPLEMENTATION
+#include "vk_mem_alloc.h"
+
+#pragma warning(pop)
 
 #include <vector>
 
